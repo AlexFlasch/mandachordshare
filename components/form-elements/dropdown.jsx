b5ac18f7325949411e1c66d80347f5de18040f7d
Flascher <alexanderflasch@gmail.com>
2018-10-16 17:24:00 -0700

    Began work on dropdown form element

---
+++ components/form-elements/dropdown.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+import palette from '../../palette';
+
+const DropdownBox = styled.div`
+  background: none;
+  border: none;
+  border-bottom: 3px solid ${palette.lotusTheme.accent};
+  box-sizing: border-box;
+  color: ${palette.lotusTheme.secondary};
+  font-family: inherit;
+  height: 35px;
+  
+`;
+
+export default (props) => {
+
+}
