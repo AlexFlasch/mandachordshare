059b5a243a7fc486bac54e68ce319f571322454b
Flascher <alexanderflasch@gmail.com>
2018-11-23 20:34:53 -0800

    More changes to get dropdown working, added prettier and eslint

--- components/form-elements/dropdown.jsx
+++ components/form-elements/dropdown.jsx
@@ -1,7 +1,7 @@
-import { createRef } from 'react';
-import styled from 'styled-components';
+import { createRef } from "react";
+import styled from "styled-components";
 
-import palette from '../../palette';
+import palette from "../../palette";
 
 const DropdownBox = styled.div`
   background: none;
@@ -10,7 +10,7 @@ const DropdownBox = styled.div`
   box-sizing: border-box;
   color: ${palette.lotusTheme.primary};
   cursor: pointer;
-  font-family: 'Teko', sans-serif;
+  font-family: "Teko", sans-serif;
   font-size: 1.5em;
   height: 35px;
   padding: 5px;
@@ -20,7 +20,7 @@ const DropdownBox = styled.div`
     border-left: 10px solid transparent;
     border-right: 10px solid transparent;
     border-top: 10px solid ${palette.lotusTheme.accent};
-    content: '';
+    content: "";
     height: 0;
     position: absolute;
     right: 5px;
@@ -33,7 +33,10 @@ const DropdownBox = styled.div`
 const DropdownList = styled.div`
   background-color: ${palette.lotusTheme.bg};
   border: 1px solid ${palette.lotusTheme.primary};
-  display: none;
+  display: ${props => {
+    console.log(`dropdown list visibility: ${props.isVisible} `);
+    props.isVisible ? 'flex' : 'none';
+  }};
   flex-direction: column;
   left: 0;
   position: absolute;
@@ -42,68 +45,63 @@ const DropdownList = styled.div`
   z-index: 1;
 `;
 
-export default (props) => {
-
+export default props => {
   let selectedValue = undefined;
-  let dropdownEl = createRef();
   let listEl = createRef();
   let isOpen = false;
 
   const selectItem = (name, value) => {
     selectedValue = { name, value };
-    listEl.current.style.display = '';
     isOpen = false;
-  }
+  };
 
   const getDropdownItems = () => {
-    return props.items
-      ? props.items.map((item, i) => 
+    return props.items ? (
+      props.items.map((item, i) => (
         <DropdownItem
           key={i}
           name={item.name}
           value={item.value}
           onClick={() => selectItem(item.name, item.value)}
         />
-      )
-      : <DropdownItem name={'No available items'} value={-1}/>
-  }
+      ))
+    ) : (
+      <DropdownItem name={"No available items"} value={-1} />
+    );
+  };
 
   const toggleListVisibility = ({ currentTarget }) => {
     if (isOpen) {
       isOpen = false;
     } else {
       isOpen = true;
-      listEl.current.style.display = 'flex';
-      document.body.addEventListener('click', (e) => {
-        if (listEl.current.contains(e.target)) {
-          return;
-        } else {
-          listEl.current.style.display = '';
-          setTimeout(() => {
-            // timeout required otherwise this click handler
-            // runs before toggleListVisibility does again
-            isOpen = false;
-          }, 100);
-        }
-      }, { once: true });
+      document.body.addEventListener(
+        "click",
+        e => {
+          if (listEl.current.contains(e.target)) {
+            return;
+          } else {
+            setTimeout(() => {
+              // timeout required otherwise this click handler
+              // runs before toggleListVisibility runs again
+              isOpen = false;
+            }, 100);
+          }
+        },
+        { once: true }
+      );
     }
-  }
+  };
 
   return (
-    <DropdownBox
-      innerRef={dropdownEl}
-      className={props.className}
-      onClick={toggleListVisibility}
-    >
-      { selectedValue || props.placeholder || 'Select a value' }
-      <DropdownList 
-        innerRef={listEl}
-      >
-        { getDropdownItems() }
+    <DropdownBox className={props.className} onClick={toggleListVisibility}>
+      {selectedValue || props.placeholder || "Select a value"}
+      <DropdownList innerRef={listEl} isVisible={isOpen}>
+        {getDropdownItems()}
       </DropdownList>
     </DropdownBox>
   );
-}
+};
 
 const ItemContainer = styled.span`
   padding: 2px;
@@ -115,11 +113,5 @@ const ItemContainer = styled.span`
 `;
 
 const DropdownItem = ({ name, onClick }) => {
-  return (
-    <ItemContainer
-      onClick={() => onClick()}
-    >
-      {name}
-    </ItemContainer>
-  );
-}
+  return <ItemContainer onClick={onClick}>{name}</ItemContainer>;
+};
