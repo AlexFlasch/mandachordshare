6d0337b988499d9bf7840e145452e0e2c39dfcd1
AlexFlasch <alexanderflasch@gmail.com>
2019-11-03 16:04:37 -0800

    in progress switching to ToneJS Sequence.

--- state/sagas/mandachord.js
+++ state/sagas/mandachord.js
@@ -19,6 +19,7 @@ import {
 import Tone from 'tone';
 
 let audioScheduler;
+let sequence;
 
 // function* playSong() {
 //   const currentStep = yield select(selectCurrentStep);
@@ -35,39 +36,74 @@ let audioScheduler;
 //   lastStep = currentStep;
 // }
 
-function* initializeAudioScheduler() {
-  audioScheduler = yield call(createAudioScheduler);
+// function* initializeAudioScheduler() {
+//   audioScheduler = yield call(createAudioScheduler);
+// }
+
+// function* playPauseAudioScheduler() {
+//   const isPlaying = yield select(selectIsPlaying);
+
+//   yield call(audioScheduler.playPause, isPlaying);
+// }
+
+// function* updateAudioSchedulerSteps() {
+//   const song = yield select(selectNotesAsSong);
+
+//   console.log('sending all steps to scheduler: ', song);
+//   yield call(audioScheduler.updateAllSteps, song);
+// }
+
+// function* updateAudioSchedulerCurrentStep() {
+//   const currentStep = yield select(selectCurrentStep);
+
+//   yield call(audioScheduler.updateCurrentStep, currentStep);
+// }
+
+// function* updateAudioSchedulerInstruments() {
+//   const instrumentSprites = yield select(selectInstrumentSprites);
+
+//   yield call(audioScheduler.updateInstruments, instrumentSprites);
+// }
+
+const createSequence = () => {
+  // array from 1 to 64
+  const noteEvents = [...Array(64).keys()].map(val => val + 1);
+
+  Tone.Transport.start();
+  sequence = new Tone.Sequence(
+    (time, col) => {
+      console.log('time: ', time);
+      console.log('col: ', col);
+    },
+    noteEvents,
+    '16n'
+  ).start('+0.1n');
+  // create a 64 column step sequencer, with each column being a 16th note
+  // start(0) tells the sequencer to start playing at time 0 when its played
+};
+
+const playPauseSequencer = async () => {
+  if (Tone.context.state === 'suspended') {
+    await Tone.context.resume();
+  }
+};
+
+function* initializeSequencer() {
+  yield call(createSequence);
 }
 
-function* playPauseAudioScheduler() {
-  const isPlaying = yield select(selectIsPlaying);
-
-  yield call(audioScheduler.playPause, isPlaying);
+function* playPauseSaga() {
+  yield call(playPauseSequencer);
 }
 
-function* updateAudioSchedulerSteps() {
-  const song = yield select(selectNotesAsSong);
-
-  console.log('sending all steps to scheduler: ', song);
-  yield call(audioScheduler.updateAllSteps, song);
-}
-
-function* updateAudioSchedulerCurrentStep() {
-  const currentStep = yield select(selectCurrentStep);
-
-  yield call(audioScheduler.updateCurrentStep, currentStep);
-}
-
-function* updateAudioSchedulerInstruments() {
-  const instrumentSprites = yield select(selectInstrumentSprites);
-
-  yield call(audioScheduler.updateInstruments, instrumentSprites);
-}
+function* updateToneTransports() {}
 
 export default function* mandachordSaga() {
-  yield takeLatest(SET_CLIENT_IS_LOADED, initializeAudioScheduler);
-  yield takeLatest(CHANGE_INSTRUMENT, updateAudioSchedulerCurrentStep);
-  yield takeLatest(UPDATE_PLAYBACK_TIME, updateAudioSchedulerCurrentStep);
-  yield takeLatest(PLAY_PAUSE, playPauseAudioScheduler);
-  yield takeLatest(TOGGLE_NOTE, updateAudioSchedulerSteps);
+  yield takeLatest(SET_CLIENT_IS_LOADED, initializeSequencer);
+  // yield takeLatest(CHANGE_INSTRUMENT, updateAudioSchedulerCurrentStep);
+  // yield takeLatest(UPDATE_PLAYBACK_TIME, updateAudioSchedulerCurrentStep);
+  yield takeLatest(PLAY_PAUSE, playPauseSaga);
+  // yield takeLatest(TOGGLE_NOTE, updateAudioSchedulerSteps);
+
+  yield takeLatest(TOGGLE_NOTE, updateToneTransports);
 }
